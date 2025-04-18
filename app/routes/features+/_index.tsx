9b6fb49b5de4a2be918c999d7a71ad788ccbb3af
Carlos Hernandez <carlosandresh925@gmail.com>
2025-04-17 22:10:10 -0700

    update features page

--- app/routes/features+/_index.tsx
+++ app/routes/features+/_index.tsx
@@ -1,17 +1,33 @@
 export default function Component() {
     return (
         <div>
-            <div className="pt-8 text-white">
-                <div className="flex flex-row w-full place-content-center pb-8">
+            <div className="text-white">
+                <div className="flex flex-row w-full place-content-center py-12">
                     <h1 className="text-4xl font-custom text-red-900">
                         Features
                     </h1>
                 </div>
-                <div className="flex flex-row w-full">
+                <div className="flex flex-row w-full pb-12">
                     <div className="flex flex-row w-1/2 justify-start pl-8">
-                        <img src="/images/inventory.png" alt="William N Pennington Engineering Building" className="w-full" />
+                        <img src="/images/showcaseMap.png" alt="William N Pennington Engineering Building" className="w-full" />
                     </div>
                     <div className="flex flex-row w-1/2 justify-end pr-8">
+                        <div className="flex flex-col w-full space-y-4 justify-center items-center">
+                            <h1 className="text-3xl font-custom">
+                                Beautiful Maps
+                            </h1>
+                            <div className="flex text-wrap px-8 pt-4">
+                                Using procedural content generation, we are able to create large, beautiful maps that are both fun to explore and easy to navigate.
+                                The maps are designed to be visually stunning, with a variety of biomes and environments that players can explore and that add to the game's
+                                storyline. Navigate through lust forests or frozen tundras, and discover the secrets hidden within each map. Players will begin their
+                                journey constrained to a small area, where they must kill enemies, complete objectives, and collect items in order to progress and 
+                                unlock new areas.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-row w-full py-12">
+                    <div className="flex flex-row w-1/2 justify-end pl-8">
                         <div className="flex flex-col w-full space-y-4 justify-center items-center">
                             <h1 className="text-3xl font-custom">
                                 Inventory
@@ -24,6 +40,25 @@ export default function Component() {
                                 encumbered: a state where they cannot move quickly. Additionally, players wll be able to collect money dropped by enemies and use it to purchase
                                 items from vendors throughout the game world. A real time projection of the player character will be displayed in the inventory screen, 
                                 allowing players to see how their character looks with the items they have equipped.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-1/2 justify-start pr-8">
+                        <img src="/images/inventory.png" alt="William N Pennington Engineering Building" className="w-full" />
+                    </div>
+                </div>
+                <div className="flex flex-row w-full py-12">
+                    <div className="flex flex-row w-1/2 justify-start pl-8">
+                        <img src="/images/combat.png" alt="William N Pennington Engineering Building" className="w-full" />
+                    </div>
+                    <div className="flex flex-row w-1/2 justify-end pr-8">
+                        <div className="flex flex-col w-full space-y-4 justify-center items-center">
+                            <h1 className="text-3xl font-custom">
+                                Combat
+                            </h1>
+                            <div className="flex text-wrap px-8 pt-4">
+                                Our game features a dynamic combat system that allows players to engage in fast-paced, action-packed battles with a variety of enemies.
+                                Players will have access to a wide range of weapons ranging from swords to cooking pans to staffs, all with their own drawbacks and benefits. 
                             </div>
                         </div>
                     </div>
