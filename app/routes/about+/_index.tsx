dedaa87f87aa4f91c6672b70fe7fc1040664ff10
Carlos Hernandez <carlosandresh925@gmail.com>
2025-04-17 20:22:15 -0700

    update meet the team page

--- app/routes/about+/_index.tsx
+++ app/routes/about+/_index.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useRef } from "react";
+
 export default function Component() {
+    const contentRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const observer: IntersectionObserver = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('fly-up-animation');
+                        observer.unobserve(entry.target);
+                    }
+                });
+            },
+            {
+                threshold: 0.1,
+            }
+        );
+
+        if (contentRef.current) {
+            observer.observe(contentRef.current);
+        }
+
+        return () => {
+            if (contentRef.current) {
+                observer.unobserve(contentRef.current);
+            }
+        }
+    }, []);
+
     return (
         <div>
-            <div className="flex flex-row place-content-center text-white">
+            <style>
+                {
+                    `
+                    @keyframes flyUp {
+                        0% {
+                            opacity: 0;
+                            transform: translateY(100px);
+                        }
+                        100% {
+                            opacity: 1;
+                            transform: translateY(0);
+                        }
+                    }
+                    .fly-up-animation {
+                        animation: flyUp 2s ease-out forwards;
+                    }
+                    .hidden-initially {
+                        opacity: 0;
+                    }
+                    `
+                }
+            </style>
+            <div ref={contentRef} className="flex flex-row place-content-center text-white hidden-initially">
                 <div className="w-2/3 pt-8">
                     <div className="flex flex-row place-content-center pb-8">
                         <h1 className="text-5xl font-custom text-red-900">
--- app/routes/about+/theTeam.tsx
+++ app/routes/about+/theTeam.tsx
@@ -2,15 +2,15 @@ export default function Component() {
     return (
         <div>
             <div className="flex flex-row place-content-center w-full text-white">
-                <div className="w-full pt-4 space-y-6">
+                <div className="w-full pt-4 space-y-12">
                     <div className="flex flex-row place-content-center w-full">
                         <h1 className="text-4xl font-custom text-red-900">
                             The Devs, Team 10
                         </h1>
                     </div>
-                    <div className="flex flex-row w-full pt-12">
-                        <div className="flex flex-row w-1/2 justify-center pl-8">
-                            <img src="/images/chillguy.JPG" alt="chill guy" className="w-1/3" />
+                    <div className="flex flex-row w-full pt-12 bg-gray-800 rounded-3xl">
+                        <div className="flex flex-row w-1/2 justify-center pl-8 pb-12">
+                            <img src="/images/tristan.png" alt="chill guy" className="w-1/3" />
                         </div>
                         <div className="flex flex-row w-1/2 justify-end pr-8">
                             <div className="flex flex-col w-full space-y-4 items-center">
@@ -18,34 +18,38 @@ export default function Component() {
                                     Tristan Hughes
                                 </h1>
                                 <div className="flex text-wrap px-8 pt-4">
-                                    I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                    with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                    several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                    the development of this game.
+                                    Hello! My name is Tristan Hughes & I am a senior at UNR studying Computer Science. I have a passion for
+                                    game development and have worked on several projects in this area. I am enrolled in the universities Digital 
+                                    Interactive Games minor program to get a better grasp at all the in's and out's of game development. In regards
+                                    to technical skills, I have experience with Unity, Unreal Engine, and C++. I am excited to be 
+                                    part of the Fallen Society team and contribute to creating a fun & engaging game for all of you to enjoy.
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row w-full pt-12">
+                    <div className="flex flex-row w-full pt-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-row w-1/2 justify-end pl-8">
                             <div className="flex flex-col w-full space-y-4 items-center">
                                 <h1 className="text-3xl font-custom">
                                     Carlos Hernandez
                                 </h1>
                                 <div className="flex text-wrap px-8 pt-4">
-                                    I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                    with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                    several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                    the development of this game.
+                                    Hello, my name is Carlos Hernandez & I am a senior here at UNR studying Computer Science. My main interests
+                                    within the field involve Front-End Development & Enterprise Software Development. You might be thinking, building a
+                                    game is very different than these interests, and you'd be correct. My reason for choosing to build a game revolves
+                                    around the fact that I feel that contributing to this project will and has
+                                    been a great opportunity to learn more about team collaboration, project management, and game development. I am excited to
+                                    be contributing to Fallen Society as building a game has always been something I wanted to do, and I hope you all enjoy
+                                    playing our game as much as we enjoyed building it!
                                 </div>
                             </div>
                         </div>
-                        <div className="flex flex-row w-1/2 justify-center pr-8">
-                            <img src="/images/chillguy.JPG" alt="chill guy" className="w-1/3" />
+                        <div className="flex flex-row w-1/2 justify-center pr-8 pb-16">
+                            <img src="/images/carlos.jpeg" alt="chill guy" className="w-1/3" />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full pt-12">
-                        <div className="flex flex-row w-1/2 justify-center pl-8">
+                    <div className="flex flex-row w-full pt-12 bg-gray-800 rounded-3xl">
+                        <div className="flex flex-row w-1/2 justify-center pl-8 pb-12">
                             <img src="/images/chillguy.JPG" alt="chill guy" className="w-1/3" />
                         </div>
                         <div className="flex flex-row w-1/2 justify-end pr-8">
@@ -54,10 +58,10 @@ export default function Component() {
                                     Joey Bertrand
                                 </h1>
                                 <div className="flex text-wrap px-8 pt-4">
-                                    I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                    with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                    several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                    the development of this game.
+                                    Hello! My name is Joey Bertrand and I am a senior at UNR studying Computer Science. My biggest interest within the
+                                    field is game development, and I have been working on several projects in this area. Working on Fallen Society has proven
+                                    to be a great opportunity to learn more about game development and how to work in a team. I have experience with Unity, Unreal Engine,
+                                    and C++ and I am excited to continue working on this project even after the semester ends. I hope you all enjoy our game!
                                 </div>
                             </div>
                         </div>
@@ -67,22 +71,24 @@ export default function Component() {
                             Our Advisor
                         </h1>
                     </div>
-                    <div className="flex flex-row w-full pt-12">
+                    <div className="flex flex-row w-full pt-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-row w-1/2 justify-end pl-8">
                             <div className="flex flex-col w-full space-y-4 items-center">
                                 <h1 className="text-3xl font-custom">
-                                    Dr. Eelke Folmer, Chair of CSE at UNR
+                                    <a href="https://www.unr.edu/cse/people/eelke-folmer" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
+                                        Dr. Eelke Folmer, CSE Department Chair
+                                    </a>
                                 </h1>
                                 <div className="flex text-wrap px-8 pt-4">
-                                    I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                    with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                    several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                    the development of this game.
+                                    Dr. Eelke Folmer received his Ph.D. in software engineering from the University of Groningen in 2005. 
+                                    He was a postdoc at the University of Alberta in 2005. Since 2006 he has been faculty in the Department 
+                                    of Computer Science and Engineering at the University of Nevada, Reno, mostly working on human-computer interaction, 
+                                    i.e., accessibility of video games, wearables and indoor navigation.
                                 </div>
                             </div>
                         </div>
-                        <div className="flex flex-row w-1/2 justify-center pr-8">
-                            <img src="/images/chillguy.JPG" alt="chill guy" className="w-1/3" />
+                        <div className="flex flex-row w-1/2 justify-center pr-8 pb-12">
+                            <img src="/images/drfolmer.png" alt="chill guy" className="w-1/3" />
                         </div>
                     </div>
                     <div className="flex flex-row place-content-center w-full pt-8 pb-4">
@@ -90,55 +96,52 @@ export default function Component() {
                             The Teaching Team
                         </h1>
                     </div>
-                    <div className="flex flex-row w-full justify-center pt-6">
+                    <div className="flex flex-row w-full justify-center pt-6 pb-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-col w-1/2 space-y-4 items-center">
                             <h1 className="text-3xl font-custom">
-                                Dr. Dave Feil Seifer, Professor in CSE at UNR
+                                <a href="https://www.unr.edu/cse/people/david-feil-seifer" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
+                                    Dr. Dave Feil Seifer, Professor in CSE at UNR
+                                </a>
                             </h1>
                             <div className="flex text-wrap px-8 pt-4">
-                                I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                the development of this game.
+                                Dr. David Feil-Seifer studied computer science for his undergraduate degree at the University of Rochester, M.S. and Ph.D. 
+                                at the University of Southern California, and postdoctoral associate at Yale University. He was the winner of the Association 
+                                for the Advancement of Artificial Intelligence (AAAI) Robot Host Competition (2002). He received the Mellon Mentoring award in 2007. 
+                                In 2011, he was awarded a National Science Foundation Computing Innovation Fellowship, the USC Order of Areté. In 2012, he received the 
+                                USC Engineering Best Dissertation Award.
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row w-full justify-center pt-6">
+                    <div className="flex flex-row w-full justify-center pt-6 pb-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-col w-1/2 space-y-4 items-center">
                             <h1 className="text-3xl font-custom">
                                 Sara Davis, Lecturer in CSE at UNR
                             </h1>
                             <div className="flex text-wrap px-8 pt-4">
-                                I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                the development of this game.
+                                Sara Davis is a lecturer in the Department of Computer Science and Engineering at the University of Nevada, Reno.
+                                She is a current Ph.D candidate within the CSE Department and has experience in industry as well as experience
+                                teaching various levels of Computer Science courses.
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row w-full justify-center pt-6">
+                    <div className="flex flex-row w-full justify-center pt-6 pb-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-col w-1/2 space-y-4 items-center">
                             <h1 className="text-3xl font-custom">
                                 Vinh Le, Graduate Student and Lecturer in CSE at UNR
                             </h1>
                             <div className="flex text-wrap px-8 pt-4">
-                                I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                the development of this game.
+                                Vinh Le is a graduate student in the Department of Computer Science and Engineering at the University of Nevada, Reno.
+                                He has experience lecturing high level Computer Science courses such as CS 425 & 426 and has much experience in industry.
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row w-full justify-center pt-6 pb-12">
+                    <div className="flex flex-row w-full justify-center pt-6 pb-12 bg-gray-800 rounded-3xl">
                         <div className="flex flex-col w-1/2 space-y-4 items-center">
                             <h1 className="text-3xl font-custom">
                                 Levi Scully, TA for CS 425 and 426 at UNR
                             </h1>
                             <div className="flex text-wrap px-8 pt-4">
-                                I am a senior at the University of Nevada, Reno studying Computer Science. I am a software engineer
-                                with experience in C++, Java, and Python. I have a passion for game development and have worked on
-                                several projects in this area. I am excited to be part of the Fallen Society team and contribute to
-                                the development of this game.
+                                Levi Scully is the TA for CS 425 and 426 at the University of Nevada, Reno.
                             </div>
                         </div>
                     </div>
