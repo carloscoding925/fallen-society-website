5413bfa73467cf1d7b71fca84c836c32c372f44d
Carlos Hernandez <carlosandresh925@gmail.com>
2025-04-18 20:01:10 -0700

    update resources page

--- app/routes/about+/resources.tsx
+++ app/routes/about+/resources.tsx
@@ -30,6 +30,28 @@ export default function Component() {
                             <a href="https://www.gameenginebook.com/" target="_blank" rel="noopener noreferrer" className="font-bold text-lg hover:text-blue-400">Game Engine Architecture (Click me)</a>
                         </div>
                     </div>
+                    <div className="flex flex-row place-content-center w-full pt-6 pb-8">
+                        <h1 className="text-2xl font-custom text-white">
+                            Related News
+                        </h1>
+                    </div>
+                    <div className="flex flex-row place-content-center">
+                        <div className="flex flex-col w-1/2 space-y-6 justify-center items-center">
+                            <a href="https://www.unrealengine.com/en-US/blog/unreal-engine-5-5-is-now-available" target="_blank" rel="noopener noreferrer" className="font-bold text-lg hover:text-blue-400">Unreal Engine 5.5 Release - News, Features, and Updates (Click me)</a>
+                        </div>
+                    </div>
+                    <div className="flex flex-row place-content-center w-full pt-6 pb-8">
+                        <h1 className="text-2xl font-custom text-white">
+                            Related Articles, Journals, and Documentation
+                        </h1>
+                    </div>
+                    <div className="flex flex-row place-content-center">
+                        <div className="flex flex-col w-1/2 space-y-6 justify-center items-center">
+                            <a href="https://dev.epicgames.com/documentation/en-us/unreal-engine/unreal-engine-5-5-documentation" target="_blank" rel="noopener noreferrer" className="font-bold text-lg hover:text-blue-400">Unreal Engine Documentation (Click me)</a>
+                            <a href="https://digra.org/" target="_blank" rel="noopener noreferrer" className="font-bold text-lg hover:text-blue-400">Digital Games Research Association (Click me)</a>
+                            <a href="https://users.cs.northwestern.edu/~hunicke/MDA.pdf" target="_blank" rel="noopener noreferrer" className="font-bold text-lg hover:text-blue-400">A Formal Approach to Game Design & Game Research (Click me)</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
